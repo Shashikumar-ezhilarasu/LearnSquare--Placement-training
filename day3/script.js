1774ab430a443c348768f7328c36fb847dc28a82
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2025-12-05 15:36:52 +0530

    understanding of JSON

--- day3/script.js
+++ day3/script.js
@@ -48,8 +48,60 @@ function check(){
     };  
 }
 
+//API Fetch Request using the GET method
+// let options = {
+//     method: 'GET',
+// };
+
+// fetch('https://gorest.co.in/public/v2/users', options)
+//     .then(response => response.json())
+//     .then(function(jsonData) {
+//         console.log(jsonData);
+//     })
+//     .catch(err => console.error('error: ' + err));
+// End of API Fetch Request
+
+// let data = {
+//     Name :"Shashikumar",
+//     class:"CSE",
+//     reg:"094"
+// }
+
+// let options = {
+//     method: 'POST',
+//     headers: {
+//         'Content-Type': 'application/json',
+//         Accept: 'application/json',
+//     Authorization:"Bearer a93365050bfd7180f78d749edbbf9efe7d8d9eb0e35682a6b3b023984bcad4f3"
+//     },
+//     body: JSON.stringify(data);
+// };
+
+
+// fetch('https://gorest.co.in/public/v2/users', oprions)
+//     .then(response => response.json())
+//     .then(function(jsonData) {
+//         console.log(jsonData);
+//     })
+//     .catch(err => console.error('error: ' + err));  
+
+
+
+let data = {
+    email: "Shashikumar@gmail.com",
+    name: "CSE",
+    gender: "male",
+    status: "active"
+};
+
 let options = {
-    method: 'GET',
+    method: 'POST',
+    headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Authorization': "Bearer a93365050bfd7180f78d749edbbf9efe7d8d9eb0e35682a6b3b023984bcad4f3"
+    },
+    body: JSON.stringify(data)
 };
 
 fetch('https://gorest.co.in/public/v2/users', options)
