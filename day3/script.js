6269ee85d670bb15d1308114cf28fb1319db1965
Shashikumarezhilarasu <shashikumarezhilarasu@gmail.com>
2025-12-05 12:44:46 +0530

    learning DOM

--- day3/script.js
+++ day3/script.js
@@ -18,3 +18,32 @@ function both() {
     document.getElementById("nameContainer").style.display = "none";
     document.getElementById("regContainer").style.display = "block";
 }
+
+
+let person = {
+    name: "John Doe",
+    age: 30,
+    city: "New York"
+};
+
+console.log(person.name);
+console.log(person.age);
+console.log(person.city);
+let Number = Math.ceil(Math.random() * 10);
+console.log(Number)
+function check(){
+    const nameContainer = document.getElementById("nameContainer");
+    let number = document.getElementById("number").value;
+    // let Number = Math.ceil(Math.random() * 10);
+    // console.log(Number)
+    if(number == Number){
+        nameContainer.style.display = "none";
+        document.getElementById("nameContainer").style.display = "block";
+        document.getElementById("result").style.display ="block";
+
+    }
+    else{
+        console.log("Incorrect Number");
+        
+    };  
+}
